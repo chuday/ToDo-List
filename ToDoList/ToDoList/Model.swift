cb75bef1876bcfb33650bb01032c4aff1e8b15d0
chuday <mikhail_chudaew505@mail.ru>
2021-10-23 23:09:17 +0500

    create func add-remove and save-load, add logic func add-remove items

--- ToDoList/ToDoList/Model.swift
+++ ToDoList/ToDoList/Model.swift
@@ -10,3 +10,28 @@ import Foundation
     // MVC
 // Model - model, View - TableViewController, Controller - Storyboard
 // Логика работы приложения
+
+
+var ToDoItems: [String] = ["Сделать звонок", "Запилить аппку"]
+
+// добавить айтем
+func addItem(nameItem: String) {
+    ToDoItems.append(nameItem)
+    saveData()
+}
+
+// удалить айтем
+func removeItem(at index: Int) {
+    ToDoItems.remove(at: index)
+    saveData()
+}
+
+// при вненсении изменений будем сохранять данные
+func saveData() {
+    
+}
+
+// при запуске будем загружать данные
+func loadData() {
+    
+}
